7ceaf5b6f283f73150619c1bf71c9ff7e43c98d5
dlinch <dlinch33@gmail.com>
2018-05-18 21:21:59 -0600

    added a ruby demo file

---
+++ demo/.vscode/ruby.rb
@@ -0,0 +1,38 @@
+class ExampleClass::ScopeResolution < NewScope::Operator
+
+  def initialize(options)
+    @@class_var = options[:class]
+    @instance_var = options[:instance]
+  end
+
+  def method
+    puts 'doing stuff'
+    yield if block_given?
+    other_method(:arg)
+  end
+
+  def self.class_method
+    return "I am a class method!"
+  end
+
+  private
+
+  def other_method(*args)
+    puts 'doing other stuff #{42}'
+  end
+
+  def self.private
+    [1, 2, 3].each do |item|
+      puts item
+    end
+  end
+
+  private_class_method :private
+end
+
+ExampleClass::ScopeResolution
+example_instance = ExampleClass::ScopeResolution.new(:arg)
+
+example_instance.method(:arg) do
+  puts 'yielding in block!'
+end
