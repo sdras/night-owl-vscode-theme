fdb01877f59ce844f1afc6c1a1851d08117642b6
David Reid <dreid@dreid.org>
2018-06-07 10:55:49 -0700

    Add module to demo and make sure it's italicized.

--- demo/ruby.rb
+++ demo/ruby.rb
@@ -1,37 +1,39 @@
-class ExampleClass::ScopeResolution < NewScope::Operator
+module ExampleModule
+  class ExampleClass::ScopeResolution < NewScope::Operator
 
-  def initialize(options)
-    @@class_var = options[:class]
-    @instance_var = options[:instance]
-  end
-
-  def method
-    puts 'doing stuff'
-    yield if block_given?
-    other_method(:arg)
-  end
-
-  def self.class_method
-    return "I am a class method!"
-  end
-
-  private
-
-  def other_method(*args)
-    puts 'doing other stuff #{42}'
-  end
-
-  def self.private
-    [1, 2, 3].each do |item|
-      puts item
+    def initialize(options)
+      @@class_var = options[:class]
+      @instance_var = options[:instance]
     end
-  end
 
-  private_class_method :private
+    def method
+      puts 'doing stuff'
+      yield if block_given?
+      other_method(:arg)
+    end
+
+    def self.class_method
+      return "I am a class method!"
+    end
+
+    private
+
+    def other_method(*args)
+      puts 'doing other stuff #{42}'
+    end
+
+    def self.private
+      [1, 2, 3].each do |item|
+        puts item
+      end
+    end
+
+    private_class_method :private
+  end
 end
 
-ExampleClass::ScopeResolution
-example_instance = ExampleClass::ScopeResolution.new(:arg)
+ExampleModule::ExampleClass::ScopeResolution
+example_instance = ExampleModule::ExampleClass::ScopeResolution.new(:arg)
 
 example_instance.method(:arg) do
   puts 'yielding in block!'
