ab425b3fb2bb0877b7fccbaff5028dbff0424944
Palash Mondal <palashm@live.in>
2018-06-16 10:40:55 +0530

    Added demo files used for testing

---
+++ demo/issue-91.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Dashboard extends React.Component {
+  public render() {
+    return (
+      <div>
+        Dashboard
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
