76a65f1aee530e56590881cbed767b5347dccd65
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2018-12-05 11:13:25 +0530

    fix python keywords color

--- demo/python.py
+++ demo/python.py
@@ -1,9 +1,9 @@
 from collections import deque
 
 def topo(G, ind=None, Q=[1]):
-    if ind == None:
+        ind = [0] * (len(G) + 1)  # this is a comment
         ind = [0] * (len(G) + 1) #this is a comment
-        for u in G:
+        ind = [0] * (len(G) + 1)  # this is a comment
             for v in G[u]:
                 ind[v] += 1
         Q = deque()
