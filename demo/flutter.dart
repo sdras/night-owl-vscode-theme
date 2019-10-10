e2fadef9e8339c446132e2b9df1bf111c321ffb1
Jerobastian <jero335@gmail.com>
2019-10-10 19:42:15 +0200

    "Fixed" the colors of the Dart files.

--- demo/flutter.dart
+++ demo/flutter.dart
@@ -1,34 +1,26 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as material;
 
-void main() 
-{
-  runApp
-  (
-    new MaterialApp
-    (
+void main(){  
+  runApp(
+    new MaterialApp(
       home: new MyButton(),
     )
   );
 }
 
-class MyButton extends StatefulWidget
-{
+class MyButton extends StatefulWidget{
   @override
   MyButtonState createState() => new MyButtonState();
 }
 
-class MyButtonState extends State<MyButton>
-{
+class MyButtonState extends State<MyButton>{
   String flutterText = "";
   List<String> collection = ['Flutter', 'is', 'great'];
   int index = 0;
 
-  void changeText()
-  {
-    setState
-    (
-      () 
-      {
+  void changeText(){
+    setState(
+      () {
         flutterText = collection[index];
         index++;
         index = index % 3;
@@ -37,35 +29,25 @@ class MyButtonState extends State<MyButton>
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return new Scaffold
-    (
-      appBar: new AppBar
-      (
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
         title: new Text("Stateful Widget"),
         backgroundColor: Colors.orangeAccent,
       ),
-      body: Center
-      (
-        child: new Column
-        (
+      body: Center(
+        child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>
-          [
-            new Text
-            (
+          children: <Widget>[
+            new Text(
               flutterText,
               style: new TextStyle(fontSize: 40.0)
             ),
-            new Padding
-            (
+            new Padding(
               padding: new EdgeInsets.all(10.0)
             ),
-            new RaisedButton
-            (
-              child: new Text
-              (
+            new RaisedButton(
+              child: new Text(
                 "Update",
                 style: new TextStyle(color: Colors.white)
               ),
